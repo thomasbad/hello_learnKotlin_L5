ad5b1a7f22ea28311e9d1f365bb693a41e212713
androidstudio <androidstudio@ftc1403-19.LEARNER.STUDENT>
2021-08-19 21:36:18 +0800

    Initial commit

---
+++ app/src/main/java/com/example/hello/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.hello
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var i: Int = 0
+        var b: Button
+        var tv: Textview
+    }
+}
